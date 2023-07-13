1c935eb01cc5e70442179466ac666ed09d58de11
ruolan <ruolan8@gmail.com>
2023-07-13 15:57:12 +0800

    update some new practice

---
+++ basic-language.py
@@ -0,0 +1,6 @@
+# 10_000_000_000
+age=1
+if age >= 18:
+    print('adult')
+else:
+    print('teenager')
---
+++ printvariables.py
@@ -0,0 +1,14 @@
+n=123
+f=456.789
+s1='hello,world'
+s2='hello,\'Adam\''
+s3=r'hello,''bart'''
+s4=r'''hello,
+lisa'''
+
+print(n)
+print(f)
+print(s1)
+print(s2)
+print(s3)
+print(s4)
