2e487919ee6254bb442da7144f2ea39cda4cf90b
ruolan <ruolan8@gmail.com>
2023-07-24 16:48:44 +0800

    weight practice

--- prac.py
+++ prac.py
@@ -1,7 +1,14 @@
-L=[
-    ['apple','google','microsoft'],
-    ['java','python','ruby','php'],
-    ['adam','bart','lisa']
-]
+height=1.75
+weight=80.5
+BMI=weight/(height**2)
 
-print (L[2][2])
+if BMI<18.5:
+    print('过轻')
+elif 18.5<BMI<25:
+    print('正常')
+elif 25<BMI<28:
+    print('过重')
+elif 28<BMI<32:
+    print('肥胖')
+elif BMI>32:
+    print('严重肥胖')
