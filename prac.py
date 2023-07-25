56c65517e604826d5ee69516f931e3f7a1d7ae75
ruolan <ruolan8@gmail.com>
2023-07-25 18:55:02 +0800

    practice define function

--- prac.py
+++ prac.py
@@ -1,14 +1,7 @@
-height=1.75
-weight=80.5
-BMI=weight/(height**2)
+def my_abs(x):
+    if x>=0:
+        return x
+    else:
+        return -x
 
-if BMI<18.5:
-    print('过轻')
-elif 18.5<BMI<25:
-    print('正常')
-elif 25<BMI<28:
-    print('过重')
-elif 28<BMI<32:
-    print('肥胖')
-elif BMI>32:
-    print('严重肥胖')
+print(my_abs(-99))
