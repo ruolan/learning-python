79217b266381a841a9921a8f331111db43391795
ruolan <ruolan8@gmail.com>
2023-08-18 00:21:44 +0800

    daily practice

--- prac.py
+++ prac.py
@@ -1,2 +1,7 @@
-def power(x):
-    return x*x
+class student(object):
+    def_init(self,name,score):
+        self.name=name
+        self.score=score
+    
+    def print_score(self):
+        print('%s:%s'%(self.nanme,self.score))
