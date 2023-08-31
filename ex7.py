794f346b33e0063103b38284241f6968b1d72293
ruolan <ruolan8@gmail.com>
2023-08-31 22:24:50 +0800

    more practice

---
+++ ex7.py
@@ -0,0 +1,13 @@
+print("mary had a litter lamb.")
+print("Its fleece was whtie as {}.". format('snow'))
+print("And everywhere that mary went.")
+print("."*10) # what'd that do?
+
+end1="c"
+end2="h"
+end3="e"
+end4="e"
+end5="s"
+end6="e"
+
+print(end1+end2+end3+end4+end5+end6)
